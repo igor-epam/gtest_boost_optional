04d2a8b3911e7989afdb53f5daa314d24e8916d2
Igar Yuzefovich <igar_yuzefovich@epam.com>
2021-06-25 18:17:46 +0300

    Commit with issue

---
+++ gmock_optional/gmock_optional.cpp
@@ -0,0 +1,47 @@
+﻿// gmock_optional.cpp : Defines the entry point for the application.
+//
+
+#include "gmock_optional.h"
+
+using namespace std;
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include <boost/optional/optional.hpp>
+
+class SomeClass
+{
+
+};
+
+class Tester
+{
+public:
+    virtual void Foo(boost::optional<SomeClass&> const& val) = 0;
+    virtual ~Tester() = default;
+};
+
+class MockTester : public Tester
+{
+public:
+
+    MOCK_METHOD(
+        void, Foo,
+        (boost::optional<SomeClass&> const& val), (override));
+};
+
+void Function(Tester& tester)
+{
+    tester.Foo({});
+}
+
+TEST(TesterTest, CheckOptional) {
+    MockTester mock;
+    EXPECT_CALL(mock, Foo).Times(1);
+}
+
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
